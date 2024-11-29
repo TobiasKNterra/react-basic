fd55a11685dc76b686d4b4167e6f5b1acf319649
Tobias Kaiser <158767669+TobiasKNterra@users.noreply.github.com>
2024-11-29 13:07:38 +0100

    fix double comment

--- ts-project/src/index.ts
+++ ts-project/src/index.ts
@@ -8,12 +8,6 @@ console.log("\n******* Übung 1: Funktionen und Variablen ******* \n");
 
 /**
  *
- * Übung 2: Funktionen und Variablen
- *******************************************/
-console.log("\n******* Übung 2: Funktionen und Variablen ******* \n");
-
-/**
- *
  * Übung 3: Funktionen und Variablen
  *******************************************/
 console.log("\n******* Übung 2: Funktionen und Variablen ******* \n");
